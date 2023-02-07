548f18921d59f7aec4003ca46f67d5dd1b2e7967
thangdo123 <118035774+thangdo123@users.noreply.github.com>
2023-02-07 10:26:36 +0700

    main new

--- main2.js
+++ main2.js
@@ -4,12 +4,14 @@ var app = express()
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.write('<p><a href="http://localhost:7000/student">Student</a></p>\n');
     res.end();
 })
 
 app.get('/student', (req, res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.write('<p><a href="http://localhost:7000">Back to home page</a></p>\n');
     res.end();
 })
 
